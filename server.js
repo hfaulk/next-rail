735122640f18934d581c8dabf4ccad0c38a3a25b
Harry Faulkner <hfaulkner2006@gmail.com>
2026-02-22 22:10:02 +0000

    Create routes in server.js

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-import express;
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const TOKEN = process.env.HUXLEY_TOKEN;
+const HUXLEY = "https://huxley2.azurewebsites.net";
+
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("Running on http://localhost:3000"));
+
+app.get("/api/departures/:station", async (req, res) => {
+  try {
+    const url =
+      HUXLEY + "/departures/" + req.params.station + "/10?accessToken=" + TOKEN;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch departures" });
+  }
+});
+
+app.get("/api/services/:serviceId", async (req, res) => {
+  try {
+    const url =
+      HUXLEY + "/service/" + req.params.serviceId + "?accessToken=" + TOKEN;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch service" });
+  }
+});
